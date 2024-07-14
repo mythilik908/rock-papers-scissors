62d9b7a4b77348bd47a32a4eb392ce3000c5523f
Mythili <mythilik908@gmail.com>
2024-07-14 11:31:01 -0500

    first commit

---
+++ src/Game.js
@@ -0,0 +1,135 @@
+import './Game.css'
+import {useNavigate} from 'react-router-dom';
+import {useLocation} from 'react-router-dom';
+import scissor from '../src/images/icon-scissors.svg'
+import paper from '../src/images/icon-paper.svg'
+import rock from '../src/images/icon-rock.svg'
+import logo from '../src/images/logo.svg'
+import React,{ useEffect, useState } from 'react';
+import { SyncLoader } from 'react-spinners';
+import Confetti from 'react-confetti';
+import rules from '../src/images/image-rules.svg'
+import close from '../src/images/icon-close.svg'
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+
+function Game() {
+    const navigate = useNavigate();
+
+    const location = useLocation();
+    const data = location.state; 
+    var computerChoice = location.state.computerChoice;
+    const [winCount, setWinCount] = useState(0);
+
+    const[showComputerChoice,setComputerChoice] = useState(false);
+    const[showSpinner,setShowSpinner] = useState(true);
+
+    const [result, setResult] = useState("");
+
+    const width = window.innerWidth;
+    const height = window.innerHeight
+
+    const [open, setOpen] = React.useState(false);
+ 
+    const handleClickToOpen = () => {
+        setOpen(true);
+    };
+ 
+    const handleToClose = () => {
+        setOpen(false);
+    };
+ 
+
+    const handleClick = () => {
+        navigate('/',{ state: { winCount }});
+      };
+
+      useEffect(() => {
+        let gameResult = "";
+        if (location.state && location.state.winCount !== undefined) {
+            setWinCount(location.state.winCount);
+        }
+        const timer = setTimeout(() => {
+            setComputerChoice(true); 
+            setShowSpinner(false);
+            if ((data.playerChoice === "rock" && computerChoice === "scissor") || 
+                (data.playerChoice === "scissor" && computerChoice === "paper") || 
+                (data.playerChoice === "paper" && computerChoice === "rock")) {
+                gameResult = "WIN"
+                setWinCount(prevCount => prevCount + 1);
+            } else if (data.playerChoice === computerChoice) {
+                gameResult = "TIE"         
+            } else {
+                gameResult  = "LOSE"
+                }
+                setResult(gameResult);
+
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, [data.playerChoice, computerChoice]);
+
+    return (
+        <>
+<div className="mytop__display">
+      <p className="logo"> <img src={logo} alt='logo'/></p>
+      <div className="myscore__display">
+        <p className="myscore__style"> SCORE </p>
+        <p className="myscore__points"> {winCount} </p>
+      </div>
+</div>
+    <div className="mybody__display">
+        <p className="myfont__text"> YOU PICKED </p>
+        <span className={data.playerChoice == "paper"? "mydot": data.playerChoice === "rock"? "mydot2":"mydot1"}></span>
+        <img className={data.playerChoice == "paper"? "mypaper": data.playerChoice === "rock"? "myscissor":"myrock"} 
+        src= {data.playerChoice === "paper"?paper:data.playerChoice === "rock"?rock:scissor}/>
+    </div>
+
+    <div className='rulesdisplay'>
+    <button className='rulesGames'  onClick={handleClickToOpen}>RULES</button>
+      <Dialog open={open} onClose={handleToClose}>
+                <DialogTitle  className='rules_font' disableTypography="true">RULES</DialogTitle>
+                <img className='close' onClick={handleToClose} src={close}/>
+                <DialogContent>
+                <img className="rules__img"src={rules}/>
+                </DialogContent>
+            </Dialog>
+    </div>
+    <div className="mycomputer__display">
+        <p className="mycomputer__text"> THE HOUSE PICKED </p>
+        <span className= {showComputerChoice == false? "mySpinner": 
+        computerChoice == "paper"? "computerdot": computerChoice === "rock"? "computerdot2":"computerdot1"  }></span> 
+         <div style={{display: !showComputerChoice? "block":"none"}}>
+        <SyncLoader className='mySpinner'
+            color={'white'}
+            loading={true}
+            size={15}
+          />  
+          </div>
+        <div style={{display: showComputerChoice? "block":"none"}}>
+        <img className= "computeroption"
+        src= {computerChoice === "paper"?paper:computerChoice === "rock"?rock:scissor}/>
+        </div>
+    </div>
+
+
+    <div style={{display: showComputerChoice? "block":"none"}} className='result'>
+{result == "WIN"?
+        <p className="mycomputer__text2"> YOU WIN </p> : 
+        result == "TIE" ?
+        <p className="mycomputer__text2"> TIE </p> : <p className="mycomputer__text2"> YOU LOSE </p> 
+}
+
+<button className='play_again'  onClick={handleClick}>PLAY AGAIN</button>
+    </div>
+
+    
+    {result == "WIN" ?<Confetti height={height} width={width} wind="200"/> : ""}
+   
+    </>
+    );
+};
+
+
+
+export default Game;
---
+++ src/Home.js
@@ -0,0 +1,87 @@
+
+import './Home.css';
+import React, { useState,useEffect } from 'react';
+import {useNavigate} from 'react-router-dom';
+import {useLocation} from 'react-router-dom';
+import {motion} from 'framer-motion';
+import triangle from '../src/images/bg-triangle.svg'
+import scissor from '../src/images/icon-scissors.svg'
+import paper from '../src/images/icon-paper.svg'
+import rock from '../src/images/icon-rock.svg'
+import logo from '../src/images/logo.svg'
+import rules from '../src/images/image-rules.svg'
+import close from '../src/images/icon-close.svg'
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+
+function Home() {
+  const navigate = useNavigate();
+
+  let arr = ["rock","paper","scissor"]
+  var computerChoice  = (arr[(Math.floor(Math.random()*arr.length))])
+
+  const [open, setOpen] = React.useState(false);
+ 
+    const handleClickToOpen = () => {
+        setOpen(true);
+    };
+ 
+    const handleToClose = () => {
+        setOpen(false);
+    };
+ 
+
+  const location = useLocation();
+  const initialWinCount = location.state ? location.state.winCount || 0 : 0; // Ensure winCount is defined
+  
+  const [winCount, setWinCount] = useState(initialWinCount);
+  
+     const handleImageClick = (e) => {
+      navigate('/game',{state:{playerChoice:e,computerChoice,winCount}});
+    };
+   
+  return (
+  <><div className="top__display">
+      <p className="logo"> <img src={logo} alt="logo"/></p>
+      <div className="score__display">
+        <p className="score__style"> SCORE </p>
+        <p className="score__points"> {winCount} </p>
+      </div>
+    </div>
+    <div className="body__display">
+      <motion.div whileHover={{ scale: 1.1 }} onHoverStart={e => {}}>
+        <span to ='/game' className="dot" onClick={()=> handleImageClick("paper")}></span>
+        <img className="paper" alt='paper' onClick={()=> handleImageClick("paper")} src={paper}/>
+        </motion.div>
+        <img className='triangle' alt='triangle' src={triangle}/>
+        <motion.div whileHover={{ scale: 1.1 }} onHoverStart={e => {}}>
+        <span to ='/game' className="dot1" onClick={()=> handleImageClick("scissor")} ></span>
+    <img className="scissor" alt="scissor" onClick={()=> handleImageClick("scissor")} src={scissor}/>
+    </motion.div>
+    <motion.div whileHover={{ scale: 1.1 }} onHoverStart={e => {}}>
+    <span to ='/game' className="dot2" onClick={()=> handleImageClick("rock")}></span>
+    <img className="rock" alt="rock" onClick={()=> handleImageClick("rock")} src={rock}/>
+      </motion.div>
+      </div>
+      <div className='rules__display'>
+      <button className='rules'  onClick={handleClickToOpen}>RULES</button>
+      <Dialog open={open} onClose={handleToClose}>
+                <DialogTitle  className='rules_font' disableTypography="true">RULES</DialogTitle>
+                <img className='close' onClick={handleToClose} src={close}/>
+                <DialogContent>
+                <img className="rules__img"src={rules}/>
+                </DialogContent>
+                <DialogActions>
+                   
+                </DialogActions>
+            </Dialog>
+      </div>
+      </>
+  );
+}
+
+export default Home;
+
+
